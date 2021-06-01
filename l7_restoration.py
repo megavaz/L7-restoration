bd58fb7a3d847ed355d8fcad235b308f7592e76a
megavaz <fruitpizza49@gmail.com>
2021-06-01 11:31:48 +0300

    small fixes

--- l7_restoration.py
+++ l7_restoration.py
@@ -32,63 +32,80 @@ def check_intersection(img):
     return np.count_nonzero(mask)
 
 
-def create_train_data(target_image, training_images, target_mask, train_mask, j, window_size, geospatial=True):
+def create_train_data(target_image, training_images, target_mask, train_mask, j,
+                      window_size, geospatial=True):
     target_image_window = target_image[:, j: j + window_size]
     target_mask_window = target_mask[:, j: j + window_size]
     if np.count_nonzero(target_mask_window) == 0:
         return 0, 0, 0, 0, 0
     train_mask_window = train_mask[:, j: j + window_size]
     training_images_window = training_images[:, :, j: j + window_size]
-    train_data = np.array([training_images_window[m][train_mask_window] for m in range(len(training_images_window))])
+    train_data = np.array([training_images_window[m][train_mask_window] for m in
+                           range(len(training_images_window))])
     train_label = target_image_window[train_mask_window]
     train_data = np.moveaxis(train_data, 0, 1)
     if geospatial:
         coordinates = np.array(np.where(train_mask_window))
         coordinates = np.moveaxis(coordinates, 0, 1)
         train_data = np.concatenate([train_data, coordinates], axis=1)
-    restore_data = np.array([training_images_window[m][target_mask_window] for m in range(len(training_images_window))])
+    restore_data = np.array(
+        [training_images_window[m][target_mask_window] for m in
+         range(len(training_images_window))])
     restore_data = np.moveaxis(restore_data, 0, 1)
     if geospatial:
         coordinates = np.array(np.where(target_mask_window))
         coordinates = np.moveaxis(coordinates, 0, 1)
         restore_data = np.concatenate([restore_data, coordinates], axis=1)
-    return train_data, train_label, restore_data, target_mask_window, target_image_window
+    return train_data, train_label, restore_data, \
+           target_mask_window, target_image_window
 
 
-def create_train_data_quick(target_image, training_images, target_mask, train_mask, i, j, window_size, geospatial=True):
+def create_train_data_quick(target_image, training_images, target_mask,
+                            train_mask, i, j, window_size, geospatial=True):
     target_image_window = target_image[i: i + window_size, j: j + window_size]
     target_mask_window = target_mask[i: i + window_size, j: j + window_size]
     if np.count_nonzero(target_mask_window) == 0:
         return 0, 0, 0, 0, 0
     train_mask_window = train_mask[i: i + window_size, j: j + window_size]
-    training_images_window = training_images[:, i: i + window_size, j: j + window_size]
-    train_data = np.array([training_images_window[m][train_mask_window] for m in range(len(training_images_window))])
+    training_images_window = training_images[:, i: i + window_size,
+                             j: j + window_size]
+    train_data = np.array([training_images_window[m][train_mask_window] for m in
+                           range(len(training_images_window))])
     train_label = target_image_window[train_mask_window]
     train_data = np.moveaxis(train_data, 0, 1)
     if geospatial:
         coordinates = np.array(np.where(train_mask_window))
         coordinates = np.moveaxis(coordinates, 0, 1)
         train_data = np.concatenate([train_data, coordinates], axis=1)
-    restore_data = np.array([training_images_window[m][target_mask_window] for m in range(len(training_images_window))])
+    restore_data = np.array(
+        [training_images_window[m][target_mask_window] for m in
+         range(len(training_images_window))])
     restore_data = np.moveaxis(restore_data, 0, 1)
     if geospatial:
         coordinates = np.array(np.where(target_mask_window))
         coordinates = np.moveaxis(coordinates, 0, 1)
         restore_data = np.concatenate([restore_data, coordinates], axis=1)
-    return train_data, train_label, restore_data, target_mask_window, target_image_window
+    return train_data, train_label, restore_data, \
+           target_mask_window, target_image_window
 
 
 def create_order(img):
-    number_of_skips = [np.count_nonzero(img[i] == 0) for i in range(img.shape[0])]
+    number_of_skips = [np.count_nonzero(img[i] == 0) for i in
+                       range(img.shape[0])]
     order = [i for i in range(img.shape[0])]
-    order = [x for x, _ in sorted(zip(order, number_of_skips), key=lambda pair: pair[1])]
+    order = [x for x, _ in
+             sorted(zip(order, number_of_skips), key=lambda pair: pair[1])]
     return order
 
 
 def create_horizontal_iteration(img, mask, average_segment_length=1):
     horizontal_iteration = [0]
     coordinates = np.where(mask)[1][0]
-    horizontal_iteration = horizontal_iteration + [i for i in range(coordinates, img.shape[-1], average_segment_length)]
+    horizontal_iteration = horizontal_iteration + [i for i in
+                                                   range(coordinates,
+                                                         img.shape[-1],
+                                                         average_segment_length)
+                                                   ]
     horizontal_iteration.append(img.shape[-1])
     return horizontal_iteration
 
@@ -106,8 +123,10 @@ def _dummy_restore(img_volume, mask_volume):
         if np.count_nonzero(mask) != 0:
             indexes = np.where(mask)
             (numbers, i_s, j_s) = indexes
-            for number, i, j in zip(numbers.tolist(), i_s.tolist(), j_s.tolist()):
-                img_volume[number, i, j, channel] = img_volume[number, i - 1, j, channel] if i - 1 > 0 else img_volume[
+            for number, i, j in zip(numbers.tolist(), i_s.tolist(),
+                                    j_s.tolist()):
+                img_volume[number, i, j, channel] = img_volume[
+                    number, i - 1, j, channel] if i - 1 > 0 else img_volume[
                     number, roll_down(mask_volume[number, :, j], i), j, channel]
     return img_volume
 
@@ -122,7 +141,6 @@ def _quick_restore(
         verbose=True,
         geospatial=True
 ):
-
     model = CatBoostRegressor(
         learning_rate=0.2,
         depth=5,
@@ -139,9 +157,11 @@ def _quick_restore(
             continue
         train_mask = dilation(target_mask, disk(1))
         train_mask = train_mask ^ target_mask
-        training_images = np.concatenate([img_volume[:target_image_number], img_volume[target_image_number + 1:]],
+        training_images = np.concatenate([img_volume[:target_image_number],
+                                          img_volume[target_image_number + 1:]],
                                          axis=0)
-        threshold = (target_image.shape[0] - window_size, target_image.shape[1] - window_size)
+        threshold = (target_image.shape[0] - window_size,
+                     target_image.shape[1] - window_size)
         for i in range(0, target_image.shape[0], window_size):
             for j in range(0, target_image.shape[1], window_size):
                 if i > threshold[0]:
@@ -150,7 +170,8 @@ def _quick_restore(
                     j = threshold[1]
                 for channel in range(img_volume.shape[-1]):
                     train_data, train_label, restore_data, \
-                    target_mask_window, target_image_window = create_train_data_quick(
+                    target_mask_window, \
+                    target_image_window = create_train_data_quick(
                         target_image[:, :, channel],
                         training_images[:, :, :, channel],
                         target_mask,
@@ -165,9 +186,11 @@ def _quick_restore(
                         restore_pool = Pool(restore_data)
                         res = model.predict(restore_pool)
                     except CatBoostError:
-                        res = np.full((restore_data.shape[0],), np.average(train_label[0]))
+                        res = np.full((restore_data.shape[0],),
+                                      np.average(train_label[0]))
                     target_image_window[target_mask_window] = res
-                    img_volume[target_image_number, i:i + window_size, j: j + window_size,
+                    img_volume[target_image_number, i:i + window_size,
+                    j: j + window_size,
                     channel] = target_image_window
     img_volume = np.nan_to_num(img_volume, nan=0.0)
     img_volume = np.clip(img_volume, min_value, max_value)
@@ -211,10 +234,11 @@ def _restore_images(
             target_masks.append(train_mask_2 * target_mask)
             train_masks.append((train_mask_2 ^ target_masks[-1]) * ~target_mask)
 
-
-        training_images = np.concatenate([img_volume[:target_image_number], img_volume[target_image_number + 1:]],
+        training_images = np.concatenate([img_volume[:target_image_number],
+                                          img_volume[target_image_number + 1:]],
                                          axis=0)
-        for train_mask_current, target_mask_current in zip(train_masks, target_masks):
+        for train_mask_current, target_mask_current in zip(train_masks,
+                                                           target_masks):
             horizontal_iteration = create_horizontal_iteration(
                 target_image[:, :, 0],
                 train_mask_current,
@@ -240,9 +264,11 @@ def _restore_images(
                         restore_pool = Pool(restore_data)
                         res = model.predict(restore_pool)
                     except CatBoostError:
-                        res = np.full((restore_data.shape[0],), np.average(train_label[0]))
+                        res = np.full((restore_data.shape[0],),
+                                      np.average(train_label[0]))
                     target_image_window[target_mask_window] = res
-                    img_volume[target_image_number, :, j: j + window_size, channel] = target_image_window
+                    img_volume[target_image_number, :, j: j + window_size,
+                    channel] = target_image_window
     img_volume = np.nan_to_num(img_volume, nan=0.0)
     img_volume = np.clip(img_volume, min_value, max_value)
     return img_volume
@@ -267,9 +293,11 @@ def quick_restore(
         print('Mask must have 3 dimensions (num_images, height, width)!')
 
     try:
-        assert check_intersection(mask_volume)
+        assert check_intersection(~mask_volume) == 0
     except AssertionError:
-        print('Your images do not fully cover all missing pixels! Add more images to your batch.')
+        print(
+            'Your images do not fully cover all missing pixels! Add more '
+            'images to your batch.')
 
     if len(img_volume.shape) == 3:
         img_volume = np.expand_dims(img_volume, -1)
@@ -323,22 +351,34 @@ def main():
     parser = argparse.ArgumentParser(
         description='This script performs restoration of the Landsat 7 images.'
     )
-    parser.add_argument('path_to_file', type=str, help='Path to the file you want to restore. Currently supported '
-                                                       'formats are .npz and .tif. Note that your file should have'
-                                                       'shape (num_images, height, width, num_channels).')
-    parser.add_argument('path_to_mask', type=str, help='Path to the mask. Note that the mask must be of'
-                                                       'shape (num_images, height, width). You can create proper '
-                                                       'mask with "create_mask.py".')
-    parser.add_argument('--algorithm', type=str, default='quick', choices=['quick', 'full'],
+    parser.add_argument('path_to_file', type=str,
+                        help='Path to the file you want to restore. Currently '
+                             'supported '
+                             'formats are .npz and .tif. Note that your file '
+                             'should have '
+                             'shape (num_images, height, width, num_channels).')
+    parser.add_argument('path_to_mask', type=str,
+                        help='Path to the mask. Note that the mask must be of'
+                             'shape (num_images, height, width). You can '
+                             'create proper '
+                             'mask with "create_mask.py".')
+    parser.add_argument('--algorithm', type=str, default='quick',
+                        choices=['quick', 'full'],
                         help='Type of the algorithm to use.')
     parser.add_argument('--silent', action='store_true')
     parser.add_argument('--order', type=int, action='extend', nargs='+',
-                        help='You can specify which images you want to restore and the order of restoration.'
-                             "If you don't specify this parameter, all of your images will be restored."
-                             "Note that if you use full version of the algorithm even if you specify only "
-                             "part of the images to be restored, still all of your images will be restored"
-                             "but the full version of the algorithm will be applied only to specified images."
-                             "If you don't" ' want this behavior use option "--noquick"')
+                        help='You can specify which images you want to '
+                             'restore and the order of restoration. '
+                             "If you don't specify this parameter, all of "
+                             "your images will be restored."
+                             "Note that if you use full version of the "
+                             "algorithm even if you specify only "
+                             "part of the images to be restored, still all of "
+                             "your images will be restored"
+                             "but the full version of the algorithm will be "
+                             "applied only to specified images."
+                             "If you don't" 'want this behavior use option '
+                             '"--noquick"')
     parser.add_argument('--noquick', action='store_true')
     parser.add_argument('--min', type=float, default=-1.)
     parser.add_argument('--max', type=float, default=1.)
@@ -365,9 +405,9 @@ def main():
             no_quick=args.noquick
         )
     save_image(img_volume, extension)
-    print('Restoration has ended! Result is saved in file "res.{}"'.format(extension))
+    print('Restoration has ended! Result is saved in file "res.{}"'.format(
+        extension))
 
 
 if __name__ == '__main__':
     main()
-
